aefb038085fe7fdc510c158a74ed93c611a08743
Saige Leslie <saigeleslie@gmail.com>
2018-04-25 04:17:58 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 require 'pry'
 
 class Dog
@@ -7,11 +8,24 @@ class Dog
  
   def name
     @this_dogs_name
+=======
+class Dog
+  def name=(dog_name)
+    this_dogs_name = dog_name
+  end
+ 
+  def name
+    this_dogs_name
+>>>>>>> 3812c8a13ce75782e8582b8552936afc2a050af2
   end
 end
 
 lassie = Dog.new
+<<<<<<< HEAD
 lassie.name = "Lassie"
 
 
 puts lassie.name
+=======
+lassie.name = "Lassie"
+>>>>>>> 3812c8a13ce75782e8582b8552936afc2a050af2
